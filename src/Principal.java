269d8729b3c6e783743fec2d7525d99688d838eb
Julia <juliamarques61@gmail.com>
2023-07-14 20:40:19 -0300

    feat: separando os requisitos

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,35 @@
-import classes.ImportarCSV;
+import classes.ImportarData;
 
 public class Principal {
     public static void main(String[] args) {
-        ImportarCSV importarCSV = new ImportarCSV();
+        ImportarData importarData = new ImportarData();
 
-        System.out.println(importarCSV.preenchendoListaDeFuncionarios());
-        importarCSV.imprimirLista();
+        importarData.preenchendoListaDeFuncionarios();
+        importarData.imprimirLista();
+
+        // Remover o João
+        importarData.removerFuncionario("João");
+
+        // Imprimir todos os funcionários com todas suas informaçõe
+        importarData.imprimirLista();
+
+        // 3.4 Aumento de Salario (10%)
+
+        // 3.5 Funcionários por função em um MAP, sendo a chave a “função” e o valor a “lista de funcionários”.
+
+        // 3.6 Imprimir os funcionários, agrupados por função.
+
+        // 3.8 Imprimir os funcionários que fazem aniversário no mês 10 e 12
+
+        // 3.9 Imprimir o funcionário com a maior idade, exibir os atributos: nome e idade.
+
+        // 3.10 Imprimir a lista de funcionários por ordem alfabética.
+        importarData.imprimirOrdemAlfabetica();
+
+        // 3.11 Imprimir o total dos salários dos funcionários.
+        importarData.totalDosSalarios();
+        // 3.12 Imprimir quantos salários mínimos ganha cada funcionário, considerando que o salário mínimo é R$1212.00.
+
+
     }
 }
