522d3c5daeb2454ff1c27c13bb12ccb91a52d892
Julia <juliamarques61@gmail.com>
2023-07-14 17:25:07 -0300

    refactor: alterando o nome do arquivo para ImportarData

--- src/classes/ImportarData.java
+++ src/classes/ImportarData.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ImportarCSV {
+public class ImportarData {
     List<Funcionario> listaDeFuncionarios = new ArrayList<>();
 
     public List<Funcionario> preenchendoListaDeFuncionarios() {
